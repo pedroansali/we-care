f9cd6462a145ebbd9dfe986b1ecf1b4c704be814
Pedro Ansali <pedrosl1-@hotmail.com>
2023-09-25 23:01:00 -0300

    added initial backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import * as Parse from 'parse';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
 })
 export class AppComponent {
+  constructor(){
+    Parse.initialize("DhfqZaozdGV6FKMO1RhkwOsoRACmQo1upy478yfi","yzCWazMAgDqw31lvVuZBzrKqXV7Wa6nY9WvGUsGQ"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
+    Parse.serverURL = 'https://parseapi.back4app.com/'
+  }
   title = 'WeCare';
 }
